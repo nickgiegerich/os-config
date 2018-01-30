494d74fc49cae7010a96726e5f23f5f170a49b42
Shane Panter <shane@foundationcode.com>
2018-01-30 15:10:56 -0700

    Updated to new bash prompt

--- install.sh
+++ install.sh
@@ -8,11 +8,23 @@ then
 	echo ". `pwd`/bash_settings.sh" >> $HOME/.bashrc
 fi
 
-if ! grep -q "git_prompt\.sh" ~/.bashrc
+#################################################################
+# if ! grep -q "git_prompt\.sh" ~/.bashrc		        #
+# then							        #
+# 	echo "Appending git_prompt.sh to ~/.bashrc..."	        #
+# 	echo ". `pwd`/git_prompt.sh" >> $HOME/.bashrc	        #
+# fi							        #
+#################################################################
+
+#Use super fancy prompt :)
+cd $HOME
+if [ ! -e "$HOME/.bash-git-prompt" ] 
 then
-	echo "Appending git_prompt.sh to ~/.bashrc..."
-	echo ". `pwd`/git_prompt.sh" >> $HOME/.bashrc
+	git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 fi
+
+echo 'GIT_PROMPT_ONLY_IN_REPO=1' >> $HOME/.bashrc
+echo 'source "$HOME/.bash-git-prompt/gitprompt.sh"' >> $HOME/.bashrc;
 
 #ln -s `pwd`/Xresources.dark $HOME/.Xresources
 #ln -s `pwd`/xinitrc $HOME/.xinitrc

be3a43ce2c7efbab8c5a53430ff70bcb47457628
Shane Panter <shane@foundationcode.com>
2018-04-17 10:56:23 -0600

    Updated installer

--- i3/install.sh
+++ i3/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 echo "Sym linking xinit files to git repo"
-if [ -e "$HOME/.xinitrc" ] 
+if [ -e "$HOME/.xinitrc" ]
 then
 	echo "xinitrc already linked"
 else
-	ln -s `pwd` $HOME/.xinitrc
+	ln -s `pwd`/xinitrc $HOME/.xinitrc
 fi
-echo "Finished linking .i3 files"
+echo "Finished linking xinit files"
+
+echo "Sym linking XResources files to git repo"
+if [ -e "$HOME/.XResources" ]
+then
+	echo "XResources already linked"
+else
+	ln -s `pwd`/XResources.dark $HOME/.XResources
+fi
+echo "Finished linking xinit files"
